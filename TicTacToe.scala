bc3b7ba0949be7cd1193b777da1c17ffbf5f9ed7
silkkupilkku <silvia.geier@aalto.fi>
2016-12-14 14:51:34 +0200

    Update TicTacToe.scala

--- TicTacToe.scala
+++ TicTacToe.scala
@@ -5,102 +5,73 @@ import scala.util
 import helper.TicTacToeHelper._
 
 object TicTacToe extends PApplet  {
-  
-
-  
-  val boxSize: Int = 120
+ 
+  val boxSize: Int = 140
   val boxAmount: Int = 3
   val gameSize: Int = boxAmount * boxSize
-  private var canStartNewGameTime: Option[Deadline] = None // älkää välittäkö, liittyy aikaan
-  
-  val zero = loadImage("O.png")  
-  val cross = loadImage("X.png")
-  var back = loadImage("Grass.jpg") //taustakuva
-  back.resize(gameSize,gameSize) //taustakuva oikeaan kokoon
-  var button = loadImage("start.png") // aoitusnappula XD
+  private var canStartNewGameTime: Option[Deadline] = None // älkää välittäkö tästä
+
+  // kuvien lataus ja koon muokkaus
+  var back = loadImage("Grass.jpg")
+  back.resize(gameSize,gameSize)
+  var button = loadImage("start.png")
   button.resize(100,60)
-  var deadline = loadImage("Deadline.jpg") // deadline-kuva
+  var deadline = loadImage("Deadline.jpg")
+  deadline.resize(boxSize,boxSize)
   
-  
+  //jäämiä tictactoesta, käytetään redrawImages metodissa joka on tictactoe-helperistä
   val oPlayer: String = "O"
   val xPlayer: String = "X" 
   
-  
-  private var turn = deadline
-  
+
   private var isGameOn: Boolean = false
-  
   private var begin: Boolean = true
   
   private var grid = Array.fill(boxAmount, boxAmount)("")
   
-  private var clickAmounts = 0
+  private var deadlineHits = 0 // tein nää jo valmiiks, ei tee vielä mitään
+  private var missed = 0
   
-  
-  /**
-   * The initialization method for the game.
-   */
-  
+ 
   override def setup() : Unit = { 
     size(gameSize, gameSize) 
   }
  
   
-
-  
-  /** 
-   *  The method draw is used to draw the background and the grid to the tic tac toe -game, if the game is on.
-   *  If the game has ended, the method only draws the background and a textual description of how the game ended.
-   */
-
   override def draw() : Unit = {
-    if (isGameOn) {
-      image(back,0,0) // taustakuvan asettaminen
+    if (isGameOn) { // pelinäkymä
+      image(back,0,0)
       stroke(0)
       drawGrid(this, boxAmount, boxSize)
       redrawImages(grid, deadline, oPlayer, deadline, xPlayer, boxSize, this)
-    } else if (begin) { // jäätävä aloitusnäkymä
+    } else if (begin) { // aloitusnäkymä
       image(back,0,0)
       text("Welcome to play Whack-a-Deadline!",50,50)
       image(button,130,150)
-    } else {
+    } else { // tähän pitäis tehdä häviön/voiton/tasojen välissä ilmestyvät näkymät
         image(back,0,0)
-        noLoop()
-        textSize(30)
-        fill(90)
-        text("Game over!",30,50)
-         if  (!didGameEnd(grid, oPlayer) && !didGameEnd(grid, xPlayer))  text("It's a tie!", 30, 100)
-         else if (getTurn == "O")  text ("X won :-)", 30, 100)
-         else if (getTurn == "X")  text("O won :-)", 30, 100)
       }
   }
 
-  
-  
-  /**
-   * When mouse is clicked the location is counted and if the place on the board is empty player's sign is placed there.
-   * If one player wins or the game ends in a tie (no empty boxes are left), the game can't be continued. 
-   */
-         
+ 
+ 
+   // Tällä voi nyt vaan lisäillä deadlineja, pitää korvata sillä et ne ilmestyy randomisti itsestään
   override def mouseClicked() : Unit = {
     var xCordinate: Int = mouseX / boxSize
     var yCordinate: Int = mouseY / boxSize
     if (isGameOn && grid(xCordinate)(yCordinate) == "") {
-      clickAmounts += 1
-      image(turn, xCordinate * boxSize, yCordinate * boxSize)
-      grid(xCordinate)(yCordinate) = getTurn 
-        if (didGameEnd(grid, getTurn) || clickAmounts == boxAmount * boxAmount) {
-        isGameOn = false    
-        }
-      this.changeTurn()  
+      image(deadline, xCordinate * boxSize, yCordinate * boxSize)
+      grid(xCordinate)(yCordinate) = "X" 
     }
   }
+  
+  
  
 
-  
-  override def keyPressed() { // Tyhjentää ruudun, jos siinä on deadline
+  // Poistaa deadlinet näkyvistä oikeilla näppäimillä
+  override def keyPressed() {
     if (key == ' ') isGameOn = true
-    else if (isGameOn) {
+    else if (isGameOn == true) {
       if (key == 'q' | key == 'Q') grid(0)(0) = ""
       else if (key == 'w' | key == 'W') grid(1)(0) = ""
       else if (key == 'e' | key == 'E') grid(2)(0) = ""
@@ -109,36 +80,9 @@ object TicTacToe extends PApplet  {
       else if (key == 'd' | key == 'D') grid(2)(1) = ""
       else if (key == 'z' | key == 'Z') grid(0)(2) = ""
       else if (key == 'x' | key == 'X') grid(1)(2) = ""
-      else if (key == 'c' | key == 'C') grid(2)(2) = ""
+      else if (key == 'c' | key == 'C') grid(2)(2) = ""   
       redrawImages(grid, deadline, oPlayer, deadline, xPlayer, boxSize, this)
     } 
-  }
-  
-
-  /**
-   * Returns the turn as a String.
-   */
-  
-  private def getTurn() : String = { 
-    if (turn == zero)
-      "O"
-    else "X" 
-  }
-
-  
-
-  /**
-   * Changes turn from O to X, and vice versa.
-   */
-  private def changeTurn() : Unit = { 
-    if (turn == zero)
-      turn = cross
-    else
-      turn = zero  
-  }
-  
-  def playGame() {
-    
   }
   
   
@@ -146,7 +90,7 @@ object TicTacToe extends PApplet  {
    * The main method that makes your application run and show.
    */
   def main(args: Array[String]) {
-    val frame = new javax.swing.JFrame("TicTacToe")
+    val frame = new javax.swing.JFrame("Whack-a-Deadline")
 
     frame.getContentPane().add(this)
     init
