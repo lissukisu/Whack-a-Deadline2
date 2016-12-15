c8269d84ae45fa98fcb8cab6cd290150d30b123e
silkkupilkku <silvia.geier@aalto.fi>
2016-12-15 14:19:47 +0200

    Update TicTacToe.scala

--- TicTacToe.scala
+++ TicTacToe.scala
@@ -1,33 +1,30 @@
 package kierros1
 import processing.core._
 import scala.concurrent.duration._
-import scala.util
-import helper.TicTacToeHelper._
-
-object TicTacToe extends PApplet  {
+import scala.util._
+object WhackADeadline extends PApplet  {
  
   val boxSize: Int = 140
   val boxAmount: Int = 3
   val gameSize: Int = boxAmount * boxSize
-  private var canStartNewGameTime: Option[Deadline] = None // älkää välittäkö tästä
+  private var time: Option[Deadline] = None //Some(1.seconds.fromNow) // älkää välittäkö tästä
+  val duration = Duration(1,SECONDS)
+  
+  val random = new Random
+  
 
   // kuvien lataus ja koon muokkaus
-  var back = loadImage("Grass.jpg")
+  var back = loadImage("paper.jpg")
   back.resize(gameSize,gameSize)
-  var button = loadImage("start.png")
-  button.resize(100,60)
-  var deadline = loadImage("Deadline.jpg")
+  var deadline = loadImage("icon.png")
   deadline.resize(boxSize,boxSize)
-  
-  //jäämiä tictactoesta, käytetään redrawImages metodissa joka on tictactoe-helperistä
-  val oPlayer: String = "O"
-  val xPlayer: String = "X" 
   
 
   private var isGameOn: Boolean = false
   private var begin: Boolean = true
+  private var help = false
   
-  private var grid = Array.fill(boxAmount, boxAmount)("")
+  private var grid = Array.fill(boxAmount, boxAmount)(false)
   
   private var deadlineHits = 0 // tein nää jo valmiiks, ei tee vielä mitään
   private var missed = 0
@@ -36,53 +33,77 @@ object TicTacToe extends PApplet  {
   override def setup() : Unit = { 
     size(gameSize, gameSize) 
   }
+  /*
+  def deadlineAppear: Boolean = {
+    if (time.isEmpty) false
+    else if (time.get.timeLeft <= 0.seconds) false
+    else {
+      grid(0)(0) = true
+      true
+    }
+  }
  
+  */
+  
+  
+  var onJoDeadline = {
+    false
+  }
+  
+  def newDeadline() = {
+     val x = random.nextInt(boxAmount)
+     val y = random.nextInt(boxAmount)
+     grid(x)(y) = true
+     onJoDeadline = true
+  }
+  
+
   
   override def draw() : Unit = {
     if (isGameOn) { // pelinäkymä
       image(back,0,0)
-      stroke(0)
-      drawGrid(this, boxAmount, boxSize)
-      redrawImages(grid, deadline, oPlayer, deadline, xPlayer, boxSize, this)
+      //time = Some(1.seconds.fromNow)
+      if (!onJoDeadline) newDeadline()
+      for (i <- grid.indices; j <- grid.indices) {
+       if (grid(i)(j)) {
+        image(deadline, i*boxSize, j*boxSize)
+       }
+      }
     } else if (begin) { // aloitusnäkymä
       image(back,0,0)
-      text("Welcome to play Whack-a-Deadline!",50,50)
-      image(button,130,150)
+      val myFont = createFont("GillSans-UltraBold", 32)
+      textFont(myFont)
+      fill(0)
+      text("Welcome to play\nWhack-a-Deadline!",40,100)
+    } else if (help) {
+      ???
     } else { // tähän pitäis tehdä häviön/voiton/tasojen välissä ilmestyvät näkymät
         image(back,0,0)
-      }
-  }
-
- 
- 
-   // Tällä voi nyt vaan lisäillä deadlineja, pitää korvata sillä et ne ilmestyy randomisti itsestään
-  override def mouseClicked() : Unit = {
-    var xCordinate: Int = mouseX / boxSize
-    var yCordinate: Int = mouseY / boxSize
-    if (isGameOn && grid(xCordinate)(yCordinate) == "") {
-      image(deadline, xCordinate * boxSize, yCordinate * boxSize)
-      grid(xCordinate)(yCordinate) = "X" 
     }
   }
-  
-  
- 
+
+
 
   // Poistaa deadlinet näkyvistä oikeilla näppäimillä
   override def keyPressed() {
-    if (key == ' ') isGameOn = true
-    else if (isGameOn == true) {
-      if (key == 'q' | key == 'Q') grid(0)(0) = ""
-      else if (key == 'w' | key == 'W') grid(1)(0) = ""
-      else if (key == 'e' | key == 'E') grid(2)(0) = ""
-      else if (key == 'a' | key == 'A') grid(0)(1) = ""
-      else if (key == 's' | key == 'S') grid(1)(1) = ""
-      else if (key == 'd' | key == 'D') grid(2)(1) = ""
-      else if (key == 'z' | key == 'Z') grid(0)(2) = ""
-      else if (key == 'x' | key == 'X') grid(1)(2) = ""
-      else if (key == 'c' | key == 'C') grid(2)(2) = ""   
-      redrawImages(grid, deadline, oPlayer, deadline, xPlayer, boxSize, this)
-    } 
+    if (key == ' ') {
+      isGameOn = true
+      begin = false
+    }
+    else if (isGameOn) {
+      onJoDeadline = false
+      if (key == 'q' | key == 'Q') grid(0)(0) = false
+      else if (key == 'w' | key == 'W') grid(1)(0) = false
+      else if (key == 'e' | key == 'E') grid(2)(0) = false
+      else if (key == 'a' | key == 'A') grid(0)(1) = false
+      else if (key == 's' | key == 'S') grid(1)(1) = false
+      else if (key == 'd' | key == 'D') grid(2)(1) = false
+      else if (key == 'z' | key == 'Z') grid(0)(2) = false
+      else if (key == 'x' | key == 'X') grid(1)(2) = false
+      else if (key == 'c' | key == 'C') grid(2)(2) = false  
+      
+    }
+    else if (key == 'h' | key == 'H') help = !help
   }
   
   
